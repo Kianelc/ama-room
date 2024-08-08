e9cba3c70f96f51adba7972bc66e1342ec5daed1
Kiane L. Casagrande <kiane.casagrande@gmail.com>
2024-08-08 00:11:31 -0300

    feat: Implementar método `handleSubscribe` em api.go
    Adiciona o método `handleSubscribe` que realiza as seguintes ações:
    - Valida o ID da sala fornecido.
    - Verifica a existência da sala no banco de dados.
    - Atualiza a conexão para WebSocket.
    - Gerencia assinantes e conexões de clientes.
    - Trata erros e fecha a conexão quando necessário.

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,18 +1,29 @@
 package api
 
 import (
+	"context"
+	"errors"
+	"log/slog"
 	"net/http"
+	"sync"
 
 	"github.com/Kianelc/ama-room.git/internal/store/pgstore"
-	"github.com/go-chi/chi/middleware"
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/jackc/pgx/v5"
 )
 
 //estamos adicionando o tipo concreto e adicionando aqui o ideal seria colocar como uma interface
 type apiHandler struct {
 	q *pgstore.Queries
 	r *chi.Mux
+	upgrader websocket.Upgrader
+	subscribers map[string]map[*websocket.Conn]context.CancelFunc
+	mu *sync.Mutex
 }
 
 func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +33,9 @@ func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func NweHandler(q *pgstore.Queries) http.Handler {
 	a := apiHandler {
 		q: q,
+		upgrader: websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }},
+		subscribers: make(map[string]map[*websocket.Conn]context.CancelFunc),
+		mu: &sync.Mutex{},
 	}
 
 	r := chi.NewRouter()
@@ -63,8 +77,57 @@ func NweHandler(q *pgstore.Queries) http.Handler {
 	return a
 }
 
-func (h apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request){}
-func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request){}
+func (h apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request){
+	rawRoomID := chi.URLParam(r, "room_id")
+	roomID, err := uuid.Parse(rawRoomID)
+
+	if err != nil {
+		http.Error(w, "invalid room id", http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.q.GetRoom(r.Context(), roomID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "room not found", http.StatusBadRequest)
+			return
+		}
+
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	c, err := h.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		slog.Warn("failed to upgrade connection", "error", err)
+		http.Error(w, "failed to upgrade to wa connection", http.StatusBadRequest)
+		return
+	}
+
+	defer c.Close()
+	
+	ctx, cancel := context.WithCancel(r.Context())
+
+	// concorencia e paralelismo
+	h.mu.Lock()
+	if _, ok := h.subscribers[rawRoomID]; !ok {
+		h.subscribers[rawRoomID] = make(map[*websocket.Conn]context.CancelFunc)
+	}
+	slog.Info("new client connected", "room_id", rawRoomID, "client_ip", r.RemoteAddr)
+	h.subscribers[rawRoomID][c] = cancel
+	h.mu.Unlock()
+
+	<-ctx.Done()
+
+	h.mu.Lock()
+	delete(h.subscribers[rawRoomID], c)
+	h.mu.Unlock()
+}
+
+func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request){
+
+}
+
 func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request){}
 func (h apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request){}
 func (h apiHandler) handleGetRoomMessages(w http.ResponseWriter, r *http.Request){}
